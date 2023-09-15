9090eafe0cadb04f936452ddb36adff239af2d66
pdhoangvu <pdhoangvu@gmail.com>
2023-09-15 20:01:58 +0700

    Add animation

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 function showImageById(imageId) {
-    const images = document.querySelectorAll('.slider');
-    images.forEach(image => {
-        if (image.id === imageId) {
-            image.style.display = 'block'; // Display the selected image
-        } else {
-            image.style.display = 'none'; // Hide other images
-        }
-    });
+  const images = document.querySelectorAll('.slider');
+  images.forEach((image) => {
+    if (image.id === imageId) {
+      image.style.display = 'block'; // Display the selected image
+    } else {
+      image.style.display = 'none'; // Hide other images
+    }
+  });
 }
 
 showImageById('slide1');
 
 const circles = document.querySelectorAll('.circle');
 circles.forEach((circle) => {
-    circle.addEventListener('click', () => {
-        showImageById(circle.getAttribute('data-id'));
-        
-    })
-})
+  circle.addEventListener('click', () => {
+    showImageById(circle.getAttribute('data-id'));
+    const items = document.querySelectorAll('.circle');
+    items.forEach((item) => {
+      item.classList.remove('on');
+    });
+    circle.classList.add('on');
+  });
+});
